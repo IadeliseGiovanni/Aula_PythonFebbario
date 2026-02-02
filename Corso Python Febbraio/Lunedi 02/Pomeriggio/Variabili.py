0867fc695fae741d51fe16ac018b18e03d3dc217
IadeliseGiovanni <iadelisegiovanni2000@gmail.com>
2026-02-02 17:56:05 +0100

    Add files via upload

---
+++ Corso Python Febbraio/Lunedi 02/Pomeriggio/Variabili.py
@@ -0,0 +1,19 @@
+# VARIABILI BASE 
+# nome = "Giovanni"
+# numero = 10
+# print(nome, numero)
+
+# INPUT DELL'UTENTE
+# nome = input("inserisci il tuo nome")
+# eta = int(input("inserisci la tua età"))
+# print("Ciao, " + nome + "! Benvenuto in Python!")
+
+#RACCOLTA INFORMAZIONI
+# sesso = input("Che sesso sei")
+# eta = int(input("inserisci l'età"))
+
+
+#OPERAZIONI MATEMATICHE 
+#x = 3
+#y = 5
+#print(x + y)
---
+++ Corso Python Febbraio/Lunedi 02/Variabili.py
@@ -0,0 +1,61 @@
+# VARIABILI BASE 
+'''nome = "Giovanni"
+numero = 10
+print(nome, numero)'''
+
+# INPUT DELL'UTENTE  ETA TRASFORMA STRINGA IN NUMERO PERCHé C'è int e poi input dopo
+# nome = input("inserisci il tuo nome")
+# eta = int(input("inserisci la tua età"))
+# print("Ciao, " + nome + "! Benvenuto in Python!")
+
+#RACCOLTA INFORMAZIONI
+'''sesso = input("Che sesso sei")
+eta = int(input("inserisci l'età"))'''
+
+
+#OPERAZIONI MATEMATICHE 
+'''x = 3
+y = 5
+print(x + y)'''
+
+
+# CONCATENAZIONE DI STRINGHE
+'''nome1 = "mirko"
+nome2 = "antonio"
+print (nome1 + nome2)'''
+
+# CREAZIONE DI VARIABILI INTERA E FLOAT
+'''num_int = 10
+num_float = 5.5
+print (num_int)
+print (num_float)'''
+
+#Prendere prima parola della variabile "h"
+'''h = "python"
+print(h[0])  # Risultato: p'''
+
+
+#Vari metodi print
+'''s = "Ciao , mondo"
+print(len(s))                            # Output: 10
+print(s.upper())                         # Output: CIAO MONDO
+print(s.split(' '))                      # Output: ['Ciao', 'mondo']
+print(s.replace('mondo', 'universo'))    # Output: Ciao universo
+'''
+
+#Scrivi un chat
+'''c ='a' '''#char
+
+
+#Stampa vero e falso
+'''booleeanT = True
+booleanF = False
+print (booleeanT , booleanF)'''
+
+x = 5
+y = 10 
+
+# OPERATORI DI CONFRONTO 
+print (x == y)  #Output false
+print ( x != y) #Output True
+print (x < y)   #Output True
